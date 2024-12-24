0575a016c2653cd6ee2bf7b12e29f667b6340607
Pedro Medina <pedromedina190@gmail.com>
2024-12-24 13:41:34 -0300

    add get chirps all and by id refactor a bit

---
+++ api_handlers.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/pedroomedicina/chirpy/internal/database"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
+	platform       string
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleCreateUser(w http.ResponseWriter, r *http.Request) {
+	var reqBody struct {
+		Email string `json:"email"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil || reqBody.Email == "" {
+		respondWithError(w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		return
+	}
+
+	dbUser, err := cfg.dbQueries.CreateUser(r.Context(), reqBody.Email)
+	if err != nil {
+		log.Printf("Error creating user: %v", err)
+		respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	apiUser := User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email:     dbUser.Email,
+	}
+
+	respondWithJSON(w, http.StatusCreated, apiUser)
+}
+
+func (cfg *apiConfig) validateChirp(chirp Chirp) error {
+	if len(chirp.Body) > 140 {
+		return errors.New("Chirp too long")
+	}
+
+	return nil
+}
+
+func (cfg *apiConfig) handleCreateChirp(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		respondWithError(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed))
+		return
+	}
+
+	var chirp Chirp
+	err := json.NewDecoder(r.Body).Decode(&chirp)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid JSON body")
+		return
+	}
+
+	err = cfg.validateChirp(chirp)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+	}
+
+	cleanedChirpBody := cleanProfanity(chirp.Body)
+
+	dbChirp, err := cfg.dbQueries.CreateChirp(r.Context(), database.CreateChirpParams{
+		Body:   cleanedChirpBody,
+		UserID: chirp.UserID,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	apiChirp := Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		Body:      dbChirp.Body,
+		UserID:    dbChirp.UserID,
+	}
+
+	respondWithJSON(w, http.StatusCreated, apiChirp)
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, _ *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	html := `
+	<html>
+	  <body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	  </body>
+	</html>
+	`
+	_, err := fmt.Fprintf(w, html, hits)
+	if err != nil {
+		return
+	}
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, http.StatusText(http.StatusForbidden))
+		return
+	}
+
+	err := cfg.dbQueries.DeleteAllUsers(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write([]byte("Hits counter reset to 0 and deleted all users"))
+	if err != nil {
+		return
+	}
+}
+
+func (cfg *apiConfig) handleGetAllChirps(w http.ResponseWriter, r *http.Request) {
+	chirps, err := cfg.dbQueries.GetAllChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	var apiChirps []Chirp
+	for _, chirp := range chirps {
+		apiChirps = append(apiChirps, Chirp{
+			ID:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
+		})
+	}
+
+	respondWithJSON(w, http.StatusOK, apiChirps)
+}
+
+func (cfg *apiConfig) handleGetChirpByID(w http.ResponseWriter, r *http.Request) {
+	chirpIDString := r.PathValue("id")
+	chirpID, err := uuid.Parse(chirpIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Chirp ID")
+		return
+	}
+
+	dbChirp, err := cfg.dbQueries.GetChirpByID(r.Context(), chirpID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, http.StatusText(http.StatusNotFound))
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+	respondWithJSON(w, http.StatusOK, Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		Body:      dbChirp.Body,
+		UserID:    dbChirp.UserID,
+	})
+}
--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"encoding/json"
-	"errors"
-	"fmt"
-	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/pedroomedicina/chirpy/internal/database"
@@ -14,30 +10,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"sync/atomic"
-	"time"
 )
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-	dbQueries      *database.Queries
-	platform       string
-}
-
-type User struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Email     string    `json:"email"`
-}
-
-type Chirp struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Body      string    `json:"body"`
-	UserID    uuid.UUID `json:"user_id"`
-}
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
 	w.Header().Set("Content-Type", "application/json")
@@ -73,131 +46,6 @@ func cleanProfanity(text string) string {
 	return strings.Join(words, " ")
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handleCreateUser(w http.ResponseWriter, r *http.Request) {
-	var reqBody struct {
-		Email string `json:"email"`
-	}
-
-	err := json.NewDecoder(r.Body).Decode(&reqBody)
-	if err != nil || reqBody.Email == "" {
-		respondWithError(w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
-		return
-	}
-
-	ctx := context.Background()
-	dbUser, err := cfg.dbQueries.CreateUser(ctx, reqBody.Email)
-	if err != nil {
-		log.Printf("Error creating user: %v", err)
-		respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-		return
-	}
-
-	apiUser := User{
-		ID:        dbUser.ID,
-		CreatedAt: dbUser.CreatedAt,
-		UpdatedAt: dbUser.UpdatedAt,
-		Email:     dbUser.Email,
-	}
-
-	respondWithJSON(w, http.StatusCreated, apiUser)
-}
-
-func (cfg *apiConfig) validateChirp(chirp Chirp) error {
-	if len(chirp.Body) > 140 {
-		return errors.New("Chirp too long")
-	}
-
-	return nil
-}
-
-func (cfg *apiConfig) handleCreateChirp(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		respondWithError(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed))
-		return
-	}
-
-	var chirp Chirp
-	err := json.NewDecoder(r.Body).Decode(&chirp)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid JSON body")
-		return
-	}
-
-	err = cfg.validateChirp(chirp)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, err.Error())
-	}
-
-	cleanedChirpBody := cleanProfanity(chirp.Body)
-
-	ctx := context.Background()
-	dbChirp, err := cfg.dbQueries.CreateChirp(ctx, database.CreateChirpParams{
-		Body:   cleanedChirpBody,
-		UserID: chirp.UserID,
-	})
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-		return
-	}
-
-	apiChirp := Chirp{
-		ID:        dbChirp.ID,
-		CreatedAt: dbChirp.CreatedAt,
-		UpdatedAt: dbChirp.UpdatedAt,
-		Body:      dbChirp.Body,
-		UserID:    dbChirp.UserID,
-	}
-
-	respondWithJSON(w, http.StatusCreated, apiChirp)
-}
-
-func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, _ *http.Request) {
-	hits := cfg.fileserverHits.Load()
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	html := `
-	<html>
-	  <body>
-		<h1>Welcome, Chirpy Admin</h1>
-		<p>Chirpy has been visited %d times!</p>
-	  </body>
-	</html>
-	`
-	_, err := fmt.Fprintf(w, html, hits)
-	if err != nil {
-		return
-	}
-}
-
-func (cfg *apiConfig) handleReset(w http.ResponseWriter, _ *http.Request) {
-	if cfg.platform != "dev" {
-		respondWithError(w, http.StatusForbidden, http.StatusText(http.StatusForbidden))
-		return
-	}
-
-	ctx := context.Background()
-	err := cfg.dbQueries.DeleteAllUsers(ctx)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-		return
-	}
-
-	cfg.fileserverHits.Store(0)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write([]byte("Hits counter reset to 0 and deleted all users"))
-	if err != nil {
-		return
-	}
-}
-
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -230,6 +78,8 @@ func main() {
 	mux.Handle("POST /admin/reset", http.HandlerFunc(apiCfg.handleReset))
 	mux.Handle("POST /api/chirps", http.HandlerFunc(apiCfg.handleCreateChirp))
 	mux.Handle("POST /api/users", http.HandlerFunc(apiCfg.handleCreateUser))
+	mux.Handle("GET /api/chirps", http.HandlerFunc(apiCfg.handleGetAllChirps))
+	mux.Handle("GET /api/chirps/{id}", http.HandlerFunc(apiCfg.handleGetChirpByID))
 
 	server := &http.Server{
 		Addr:    ":8080",
